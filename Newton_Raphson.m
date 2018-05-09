9d0b75a97e9442cf40c853fcbd8b0add0835e45d
kamal <kamalhabieb@yahoo.ca>
2018-05-09 19:33:04 +0200

    Newton Raphson

--- Newton_Raphson.m
+++ Newton_Raphson.m
@@ -1,4 +1,4 @@
-function [] = Newton_Raphson(x_i1,func)
+function [str_arr] = Newton_Raphson(x_i1,func)
 syms f(x)
 f(x) =func;
 fx = double(f(x_i1));
@@ -7,6 +7,14 @@ dfdx = double(dif(x_i1));
 x_i2 = x_i1-double(fx/dfdx);
 iterations = 1;
 error =abs(x_i2-x_i1);
+
+string_x_i1 = num2str(x_i1);
+string_x_i2 = num2str(x_i2);
+string_func = num2str(fx);
+string_drev = num2str(dfdx);
+string_error = num2str(error);
+str = ['x_i1 = ' string_x_i1 '  x_i2 =  ' string_x_i2 '  f(x) =  ' string_func  ' df/dx =  ' string_drev  '  error =  '  string_error];
+str_arr = {str};
 while error > .0001 && iterations < 50
     iterations = iterations+1;
     x_i1 = x_i2;
@@ -14,9 +22,16 @@ while error > .0001 && iterations < 50
     dfdx = double(dif(x_i1));
     x_i2 = x_i1-double(fx/dfdx);
     error =abs(x_i2-x_i1);
+    
+    string_x_i1 = num2str(x_i1);
+    string_x_i2 = num2str(x_i2);
+    string_func = num2str(fx);
+    string_drev = num2str(dfdx);
+    string_error = num2str(error);
+    str = ['x_i1 = ' string_x_i1 '  x_i2 =  ' string_x_i2 '  f(x) =  ' string_func  ' df/dx =  ' string_drev  '  error =  '  string_error];
+    str_arr = cat(1,str_arr,str);
+
 end
-x_i2
-iterations
 
 end
 
