d363330e489ee6093dc32603071fbc213a760c57
kamal <kamalhabieb@yahoo.ca>
2018-05-11 19:49:22 +0200

    lagrange again

--- Lagrange.m
+++ Lagrange.m
@@ -1,27 +1,48 @@
 function [ corresponding ] = Lagrange(order,values,correspondings,query )
+
+if order>=numel(values)
+   error('order is larger than number of points');
+end
+
 syms f(x)
 syms g(x)
 f(x) = 0;
-iterator_1 = 1;
-iterations = 0;
+
 values = cell2mat(values);
 correspondings = cell2mat(correspondings);
-while iterations<(order+1)
+
+iterator_1 = 1;
+index = 1;
+while query>values(iterator_1)
+    index = index+1;
+    iterator_1 = iterator_1+1;
+end
+iterator_1 = index-order
+if iterator_1<1
+    index= (1-iterator_1)+index;
+    iterator_1 = 1
+end
+iterations_1 = order+1
+iterations_2 = order
+counter_1 = 0;
+counter_2 = 0;
+while counter_1<iterations_1
     g(x) = double(correspondings(iterator_1))
-    iterator_2 = 1 ;
-    iterations_two = 1;
-    while iterations_two<(order+1)
+    iterator_2 = index-order
+    counter_2 = 0;
+    while counter_2<iterations_2
         if iterator_1 ~= iterator_2
             g(x) = g(x)*(x-values(iterator_2))/double((values(iterator_1)-values(iterator_2)))
-            iterations_two = iterations_two +1
+            counter_2 = counter_2 +1
         end
         iterator_2 = iterator_2+1
         
     end
     f(x) = f(x)+g(x);
     iterator_1 = iterator_1+1
-    iterations = iterations + 1
+    counter_1 = counter_1 + 1
 end
+
 corresponding = double(f(query))
 end
 
--- kamal_5555.m
+++ kamal_5555.m
@@ -1,9 +1,9 @@
 syms f(x)
-f(x) = exp(x)+(x^2)-x-4;
+f(x) = exp(x);
 func= matlabFunction(f);
 %FixedPoint(4,func)
-%Newton_Raphson(3.5,func)
+%Newton_Raphson(1,func,50,0.0001)
 %Secant(-1.5,-1.51,func)
 %birgeVietaCaller([2 3 1],0);
 
-Lagrange(2,{10,15,20,22.5,30},{227.04,362.78,517.35,602.97,901.67},16);
+Lagrange(6,{0,10,15,20,22.5,30},{0,227.04,362.78,517.35,602.97,901.67},16);
