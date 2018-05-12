51a89d082d5f61a6337df2d3b79199cee4d36fa7
MohamedEsmailMohamed <mesmail65525@gmail.com>
2018-05-12 02:57:39 +0200

    almost all but sum testcases dosen,t run will

--- getAllRoots.m
+++ getAllRoots.m
@@ -1,12 +1,13 @@
 function result = getAllRoots( f, maxIteration,perscision)
-    lb = -10000;
-    ub= 10000;
+    lb=-10000;
+    ub=10000;
     i=lb;
     n=1;
     u=ub;
+    co=0;
     result={};
     while i<=u &&n<50
-        
+ 
         if(f(i)==0)
             result=cat(1,result,num2str(i));
             n=n+1;
@@ -14,18 +15,21 @@ function result = getAllRoots( f, maxIteration,perscision)
            continue; 
         end
        [str1,l,l2,temp]=Newton_Raphson(i,f,maxIteration,perscision);
+       co=co+1;
        if(isnan(temp))
            q=1;
            while isnan(temp)&& i<u
            i=i+50*q;
            q=q+1;
            [str1,l,l2,temp]=Newton_Raphson(i,f,maxIteration,perscision);
-           
+                  co=co+1;
+ 
+ 
            end
        end
        c=n-1;
        while(c ~= 0)
-           if(abs(temp-result{c})<.001)
+           if(abs(temp-str2double(result{c}))<.001)
                break;
            end
            c=c-1;
@@ -35,25 +39,29 @@ function result = getAllRoots( f, maxIteration,perscision)
            n=n+1;
            i=temp+0.1;
        else
-
+ 
            i=i+(i-temp);
        end
-
+ 
     end
-    
-    l=result{1};
+ 
+    l=str2double(result{1});
+ 
     [a1,a2,a,u]=Newton_Raphson(10000,f,maxIteration,perscision);
+ 
     if(isnan(u))
         u=ub;
     end
+ 
     i=1;
     k=.5;
     while(l+k*i<u)
-        l+k*i
         if(f(l+k*i)==0)
             c=n-1;
             while(c ~= 0)
-               if(abs(l+k*i-result{c})<.0001)
+               if(abs(l+k*i-str2double(result{c}))<.001)
+ 
+ 
                    break;
                end
                c=c-1;
@@ -65,26 +73,27 @@ function result = getAllRoots( f, maxIteration,perscision)
            i=i+1;
            continue;
         end
-
-        if(f(l+k.*i)*f(l+k.*(i+1))<0)
-       [str1,l1,l2,temp]=myBisectionAlgorithm(f,l+k*i,l+k*(i+1),maxIteration,perscision);
-        c=n-1;
-       while(c ~= 0)
-           if(abs(temp-result{c})<.001)
-               break;
+ 
+        if(f(l+k*i)*f(l+k*(i+1))<0)
+ 
+           [str1,l1,l2,temp]=myBisectionAlgorithm(f,l+k*i,l+k*(i+1),maxIteration,perscision);
+            c=n-1;
+           while(c ~= 0)
+               if(abs(temp-str2double(result{c}))<.001)
+                   break;
+               end
+               c=c-1;
            end
-           c=c-1;
-       end
-       if(c==0)
-           result=cat(1,result,num2str(temp));
-           n=n+1;
-           i=temp+0.1;
-       end
+           if(c==0)
+               result=cat(1,result,num2str(temp));
+               n=n+1;
+           end
         end
         i=i+1;
     end
-        
-    
-    
+ 
+ 
+ 
+ 
+ 
 end
-
